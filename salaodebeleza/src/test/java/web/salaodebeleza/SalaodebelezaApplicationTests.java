2784080751277170918924b90d7d92d80572adae
Rangel64 <63917837+Rangel64@users.noreply.github.com>
2023-06-02 15:14:14 -0300

    Add files via upload

---
+++ salaodebeleza/src/test/java/web/salaodebeleza/SalaodebelezaApplicationTests.java
@@ -0,0 +1,13 @@
+package web.salaodebeleza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaodebelezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
